40b33fd15dc514a0372ca6af82e49f1b3c8456f4
canayglr <cvtcn-ayglr4712@hotmail.com>
2025-05-19 13:03:49 +0300

    Initial commit

---
+++ catkin_ws/build/iq_gnc/CMakeFiles/square.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/square.dir/src/square_boat.cpp.o"
+  "/home/can/catkin_ws/devel/.private/iq_gnc/lib/iq_gnc/square.pdb"
+  "/home/can/catkin_ws/devel/.private/iq_gnc/lib/iq_gnc/square"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/square.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
